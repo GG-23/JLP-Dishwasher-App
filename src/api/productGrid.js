4686733e3d0034b19800af35f13c1e40f9c640c2
Greg <greg@myschoolportal.co.uk>
2021-03-13 17:41:07 +0000

    Adding check for 20 results max

--- src/api/productGrid.js
+++ src/api/productGrid.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getProductGrid = async function (){
   return await axios.get('/search/api/rest/v2/catalog/products/search/keyword?q=dishwasher&key=AIzaSyDD_6O5gUgC4tRW5f9kxC0_76XRC8W7_mI')
     .then( response => {
-        return response.data.products    
+        return response.data.products.slice(0,20)   
     })
     .catch( () => {
         // send error to handler/tracker
--- src/api/tests/productGrid.test.js
+++ src/api/tests/productGrid.test.js
@@ -15,8 +15,31 @@ describe( 'product grid api' , () => {
         expect(axios.get).toHaveBeenCalled();   
         expect(axios.get).toHaveBeenCalledWith('/search/api/rest/v2/catalog/products/search/keyword?q=dishwasher&key=AIzaSyDD_6O5gUgC4tRW5f9kxC0_76XRC8W7_mI');          
         expect(productGrid).toEqual(mockedData.data.products);
-    })
+    });
 
+    it( 'should fetch 20 results at most', async () => {
+
+        const twentyResults = {data:{products:[]}};
+        const twentyOneResults = {data:{products:[]}};
+
+        for( let i = 0; i < 20; i++ ) {
+            const thisResult = {id: i};
+            twentyResults.data.products.push(thisResult);
+            twentyOneResults.data.products.push(thisResult);
+        }
+
+        twentyOneResults.data.products.push({id: 21});
+
+        axios.get.mockImplementationOnce(() => Promise.resolve(twentyResults));
+        const productGridTwentyPassedIn = await getProductGrid();  
+
+        expect(productGridTwentyPassedIn).toHaveLength(20);
+
+        
+        axios.get.mockImplementationOnce(() => Promise.resolve(twentyOneResults));
+        const productGridTwentyOnePassedIn = await getProductGrid();            
+        expect(productGridTwentyOnePassedIn).toHaveLength(20); 
+    })
 
     it( 'returns empty array of products if endpoint errs', async () => {
         axios.get.mockImplementationOnce(() => Promise.reject());     
