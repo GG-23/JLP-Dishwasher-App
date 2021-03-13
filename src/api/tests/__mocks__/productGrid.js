08943f0c73f3aefc8568934a27a25293bbadf487
Greg <greg@myschoolportal.co.uk>
2021-03-13 12:20:38 +0000

    Adding grid endpoint + tests

---
+++ src/api/tests/__mocks__/productGrid.js
@@ -0,0 +1,28 @@
+export default {
+    products:[{
+        productID: '1001',
+        price:{
+            was: '',
+            then1: '',
+            then2: '',
+            now: '239.00',
+            uom: '',
+            currency: 'GBP'
+        },
+        title: '',
+        image: ''
+    },
+    {
+        productID: '1002',
+        price:{
+            was: '239.00',
+            then1: '219.00',
+            then2: '209.00',
+            now: '199.00',
+            uom: '',
+            currency: 'GBP'
+        },
+        title: '',
+        image: ''
+    }]
+};
---
+++ src/api/tests/productGrid.test.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { getProductGrid } from '../productGrid';
+import mockedData from './__mocks__/productGrid';
+
+jest.mock('axios')
+axios.mockResolvedValue();
+
+describe( 'product grid api' , () => {
+
+    it( 'should fetch products from grid endpoint', async () => {
+        axios.get.mockImplementationOnce(() => Promise.resolve(mockedData));
+
+        const productGrid = await getProductGrid();   
+
+        expect(axios.get).toHaveBeenCalled();   
+        expect(axios.get).toHaveBeenCalledWith('/search/api/rest/v2/catalog/products/search/keyword?q=dishwasher&key=AIzaSyDD_6O5gUgC4tRW5f9kxC0_76XRC8W7_mI');          
+        expect(productGrid).toEqual(mockedData.products);
+    })
+
+
+    it( 'returns empty array of products if endpoint errs', async () => {
+        axios.get.mockImplementationOnce(() => Promise.reject());     
+
+        const productGrid = await getProductGrid();   
+
+        expect(axios.get).toHaveBeenCalled();   
+        expect(productGrid).toEqual([]);
+    })
+
+});
